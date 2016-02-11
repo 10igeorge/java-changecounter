ed1c021cdb01bca7b78fc4f81b6ef5747053f494
Anna Lawley and Isabelle George <ALawley1@gmail.com>
2016-02-11 09:49:38 -0800

    add unit test for negative number error handling

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -26,4 +26,9 @@ public class AppTest {
     App change = new App();
     assertEquals("Your change for 2 cents is 2 pennies.", change.getChange(2));
   }
+  @Test
+  public void getChange_forNegativeNumber_returnError(){
+    App change = new App();
+    assertEquals("Please enter a positive value", change.getChange(-2));
+  }
 }
