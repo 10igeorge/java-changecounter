65adea2224b276fc6f3a0c538ec8e6ff96bbf96c
Anna Lawley and Isabelle George <10izzygeorge@gmail.com>
2016-02-11 08:41:20 -0800

    pass first test to return quarters if change is over 25 cents

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {  }
 
+  public static String getChange(Integer change) {
+    Integer quarters = 0;
+    while (change >= 25) {
+      change -= 25;
+      quarters++;
+    }
+    return String.format("Your change for 50 cents is %d quarters.", quarters);
+  }
 
 }
