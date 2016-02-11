41a22fa1c394d5400d0037783c69d2f7664df7d9
Anna Lawley and Isabelle George <10izzygeorge@gmail.com>
2016-02-11 08:51:07 -0800

    add handling for dimes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,12 +9,19 @@ public class App {
   public static void main(String[] args) {  }
 
   public static String getChange(Integer change) {
+    Integer initialChange = change;
     Integer quarters = 0;
+    Integer dimes = 0;
     while (change >= 25) {
       change -= 25;
       quarters++;
     }
-    return String.format("Your change for 50 cents is %d quarters.", quarters);
+    while (change >= 10){
+      change -= 10;
+      dimes ++;
+    }
+    return String.format("Your change for %d cents is %d quarters, %d dimes.", initialChange, quarters, dimes);
   }
+
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,8 +6,13 @@ public class AppTest {
   @Test
   public void getChange_forNumberGreaterThan25_returnQuarters() {
     App change = new App();
-    assertEquals("Your change for 50 cents is 2 quarters.", change.getChange(50));
+    assertEquals("Your change for 50 cents is 2 quarters, 0 dimes.", change.getChange(50));
   }
 
+  @Test
+  public void getChange_forNumberGreaterThan10_returnsDimes(){
+    App change = new App();
+    assertEquals("Your change for 20 cents is 0 quarters, 2 dimes.", change.getChange(20));
+  }
 
 }
