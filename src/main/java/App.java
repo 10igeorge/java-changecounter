1e19710e92cd4d73821cad2c9522fd959496a5d7
Anna Lawley and Isabelle George <ALawley1@gmail.com>
2016-02-11 09:37:45 -0800

    create spark webpage for change output

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,31 @@
+import java.util.Map;
 import java.util.HashMap;
-
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
-
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {  }
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/change", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/change.vtl");
+
+      String userChange = request.queryParams("userChange");
+      Integer integerUserChange = Integer.parseInt(userChange);
+      String change = getChange(integerUserChange);
+
+      model.put("change", change);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public static String getChange(Integer change) {
     Integer initialChange = change;
